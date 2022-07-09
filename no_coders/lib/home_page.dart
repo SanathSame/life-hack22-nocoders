8f290b978c29d825f3163f28be1534314bbab104
Guo-KeCheng <guo.kecheng99@gmail.com>
2022-07-09 15:36:33 +0800

    Dependencies

--- no_coders/lib/home_page.dart
+++ no_coders/lib/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,7 +12,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("No Coders")),
+      body: const Center(child: Text("No Coders")),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Row(children: [
+        ElevatedButton(
+            onPressed: () {
+              selectFromImagePicker();
+            },
+            child: Text("From gallery"))
+      ]),
     );
   }
+}
+
+void selectFromImagePicker() async {
+  final ImagePicker _picker = ImagePicker();
+
+  await _picker.pickImage(source: ImageSource.gallery);
 }

118f7a3fb7bc621e4aee4b2f8cb17107c854a71f
Guo-KeCheng <guo.kecheng99@gmail.com>
2022-07-10 03:34:22 +0800

    Working main and result screen

--- no_coders/lib/image_controller.dart
+++ no_coders/lib/image_controller.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:get/get.dart';
 import 'package:no_coders/constants/constants.dart';
+import 'package:no_coders/main_screen.dart';
 
 class ImageController extends GetxController {
   String model = ssd;
@@ -10,7 +11,19 @@ class ImageController extends GetxController {
   double? imageWidth;
   double? imageHeight;
 
-  bool isLoading = false;
+  // bool isLoading = false;
 
   List? recognitions;
+
+  late String itemName;
+  late double confidenceLevel;
+
+  void renderImage() {
+    recognitions!.sort((a, b) => (a as Map).compareTo(b));
+    print(recognitions!);
+    itemName = recognitions![0]["detectedClass"];
+    confidenceLevel = recognitions![0]["confidenceInClass"] * 100;
+    print(confidenceLevel);
+    update();
+  }
 }
--- no_coders/lib/main.dart
+++ no_coders/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:no_coders/home_page.dart';
 import 'package:no_coders/dependencies.dart' as dependencies;
+import 'package:no_coders/main_screen.dart';
 
 void main() async {
   await dependencies.init();
@@ -12,13 +14,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'NoCoders',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: ScanPage(),
+      home: MainScreen(),
     );
   }
 }

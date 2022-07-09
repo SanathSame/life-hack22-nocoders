d2a3a114b33273b57af158e3d94add8697c00890
Nicholas Lee <nic.leejunyi@gmail.com>
2022-07-09 19:19:30 +0800

    added app constants

---
+++ no_coders/lib/constants/app_colours.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColours {
+  // Main Colours
+  static const Color mainColour = Color(0xFF26303D);
+  static const Color primaryTextColour = Color(0xFF26303D);
+  static const Color secondaryTextColour = Colors.white;
+  static const Color greyTextColour = Color(0xFF929292);
+  static const Color backgroundColour = Colors.white;
+  static const Color componentColour = Color(0xFFFFA518);
+  static const Color indicatorColour = Color(0XFF48BC8B);
+  static const Color greyBackgroundColor = Color(0xFFF3F3F3);
+  static Color lightgrey = Colors.grey.shade300;
+  static const Color lightOrangeCardBackground = Color(0xFFFFEED4);
+  static const Color redErrorColor = Color.fromARGB(255, 216, 66, 56);
+
+  // Tracker Colours
+  static const Color nutrition = Color(0xFFEBFAFF);
+  static const Color fitness = Color(0xFFE1E1FF);
+  static const Color focus = Color(0xFFE1E1FF);
+  // Color(0xFFF3F3F3)
+}
